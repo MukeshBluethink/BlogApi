2056156bd5bbf1d98b730ae4bc64b44c3c95e1d0
Mukesh <mukesh@bluethink.in>
2023-05-04 08:39:50 -0500

    Swagger UI added

--- src/main/java/com/springboot/blog/controller/PostController.java
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -1,5 +1,4 @@
 package com.springboot.blog.controller;
-
 import com.springboot.blog.payload.PostDto;
 import com.springboot.blog.payload.PostResponse;
 import com.springboot.blog.service.PostService;
@@ -11,9 +10,11 @@ import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @RestController
-@RequestMapping("api/v1/post")
 public class PostController {
     private PostService postService;
 
@@ -21,31 +22,57 @@ public class PostController {
         this.postService = postService;
     }
 
-    @PostMapping()
+    @PostMapping("api/v1/post")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto) {
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
     }
 
-    @GetMapping()
+    @GetMapping("api/v1/post")
     public ResponseEntity<PostResponse> getAllPost(@RequestParam(name = "pageNo", defaultValue = AppConstant.DEFAULT_PAGE_NUMBER, required = false) int pageNo, @RequestParam(name = "pageSize", defaultValue = AppConstant.DEFAULT_PAGE_SIZE, required = false) int pageSize, @RequestParam(name = "sortBy", defaultValue = AppConstant.DEFAULT_SORT_BY, required = false) String sortBy, @RequestParam(name = "sortDir", defaultValue = AppConstant.DEFAULT_SORT_DIRECTION, required = false) String sortDir) {
 
         return new ResponseEntity<>(postService.getAllPosts(pageNo, pageSize, sortBy, sortDir), HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<PostDto> getPostById(@PathVariable(name = "id") Long id) {
+    /*
+       @GetMapping(value = "/api/post/{id}" , params = "version=1")
+       Versioning using query parameter
+    */
+    @GetMapping(value = "/api/v1/post/{id}")
+    public ResponseEntity<PostDto> getPostByIdV1(@PathVariable(name = "id") Long id) {
         return ResponseEntity.ok().header("This is Header", " This is Header Value").body(postService.getPostById(id));
     }
 
+    /*
+        @GetMapping(value = "/api/post/{id}" , params = "version=2")
+        Versioning using query parameter
+     */
+/*  @GetMapping(value = "/api/post/{id}", headers = "X-API-VERSION=2")
+    public ResponseEntity<PostDtoV2> getPostByIdV2(@PathVariable(name = "id") Long id) {
+        PostDto postDto = postService.getPostById(id);
+        PostDtoV2 postDtoV2 = new PostDtoV2();
+        postDtoV2.setId(postDto.getId());
+        postDtoV2.setTitle(postDto.getTitle());
+        postDtoV2.setDescription(postDto.getDescription());
+        postDtoV2.setContent(postDto.getContent());
+        postDtoV2.setComments(postDto.getComments());
+        List<String> tags = new ArrayList<>();
+        tags.add("Java");
+        tags.add("Spring Boot");
+        tags.add("AWS");
+        postDtoV2.setTags(tags);
+        return ResponseEntity.ok().header("This is Header", " This is Header Value").body(postDtoV2);
+    }
+ */
+
     @Secured({"ROLE_ADMIN"})
-    @PutMapping("/{id}")
+    @PutMapping("/api/v1/post/{id}")
     public ResponseEntity<PostDto> updatePost(@PathVariable(name = "id") Long id, @Valid @RequestBody PostDto postDto) {
         return new ResponseEntity<>(postService.updatePost(id, postDto), HttpStatus.OK);
     }
 
     @Secured({"ROLE_ADMIN"})
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/api/v1/post/{id}")
     public ResponseEntity<String> deletePost(@PathVariable(name = "id") Long id) {
         postService.deletePostById(id);
         return new ResponseEntity<>("Post deleted success fully", HttpStatus.OK);
